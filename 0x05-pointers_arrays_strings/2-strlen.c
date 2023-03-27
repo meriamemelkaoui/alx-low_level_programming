7acc2580c8ded83eb8f64feb377e575a1e8f3638
Meriame Melkaoui <meriamemelkaoui@gmail.com>
2023-03-27 04:50:09 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ *
+ * @str: char to check
+ *
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */
+int _strlen(char *str)
+{
+	int len  = 0;
+
+	while (*str++)
+	
+		len++;
+	return (len);
+}
