35fff27810ce4df2a0dae11d33220d8361a11fe7
Meriame Melkaoui <meriamemelkaoui@gmail.com>
2023-03-27 18:04:15 -0700

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -24,13 +24,3 @@ void rev_string(char *s)
 		s[c] = d;
 	}
 }
-
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
-}
